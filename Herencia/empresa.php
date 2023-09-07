9ae64c2296a609581308b1f7ed23d05f2747a05d
PaulaSofiaG241 <125401748+PaulaSofiaG241@users.noreply.github.com>
2023-09-06 21:06:31 -0500

    Add files via upload

--- Herencia/empresa.php
+++ Herencia/empresa.php
@@ -3,9 +3,15 @@ require_once ("classPersona.php");
 require_once ("classEmpleado.php");
 require_once ("classCliente.php");
 
-$objEmpleado = new Empleado(23,"Diego",33);
+$objEmpleado = new Empleado(26,"Paula",17);
 $objEmpleado->setPuesto("Administrador");
 echo $objEmpleado->getDatosPersonales();
 echo "<br>";
 echo "Puesto: ".$objEmpleado->getPuesto();
+
+$objCliente = new Cliente (11,"Luisa",17);
+$objCliente->setCreditos(99);
+echo $objCliente->getDatosPersonales();
+echo"<br>";
+echo "Créditos: ".$objCliente->getCreditos();
 
